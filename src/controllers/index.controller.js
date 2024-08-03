f92d45af20a88751390849a36d93774f94df0670
david <dalejandro1111@gmail.com>
2024-08-03 14:56:57 -0600

    first commit

---
+++ src/controllers/index.controller.js
@@ -0,0 +1,12 @@
+import { pool } from '../db.js'
+
+export const ping = async (req, res) => {
+    try {
+        const [result] = await pool.query('SELECT 1 + 1 AS result')
+        res.json(result[0])
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Something goes wrong'
+        })
+    }
+}
